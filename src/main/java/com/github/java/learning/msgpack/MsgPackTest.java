cf3c42da4ceca0f9807b8ea7842d67127ff8d07c
guanjian23 <guanjian@jd.com>
2021-01-05 20:36:16 +0800

    feat(序列化): msgpack

--- src/main/java/com/github/java/learning/msgpack/MsgPackTest.java
+++ src/main/java/com/github/java/learning/msgpack/MsgPackTest.java
@@ -2,6 +2,8 @@ package com.github.java.learning.msgpack;
 
 import org.msgpack.MessagePack;
 import org.msgpack.template.Templates;
+import org.msgpack.type.Value;
+import org.msgpack.unpacker.Converter;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -120,7 +122,35 @@ public class MsgPackTest {
         }
     }
 
-    public static void main(String[] args) {
-        serializesPOJO();
+    public static void demo() throws IOException {
+        // Create serialize objects.
+        List<String> src = new ArrayList<String>();
+        src.add("msgpack");
+        src.add("kumofs");
+        src.add("viver");
+
+        MessagePack msgpack = new MessagePack();
+        // Serialize
+        byte[] raw = msgpack.write(src);
+        System.out.println(Arrays.toString(raw));
+
+        // Deserialize directly using a template
+        List<String> dst1 = msgpack.read(raw, Templates.tList(Templates.TString));
+        System.out.println(dst1.get(0));
+        System.out.println(dst1.get(1));
+        System.out.println(dst1.get(2));
+
+        // Or, Deserialze to Value then convert type.
+        Value dynamic = msgpack.read(raw);
+        List<String> dst2 = new Converter(dynamic)
+                .read(Templates.tList(Templates.TString));
+        System.out.println(dst2.get(0));
+        System.out.println(dst2.get(1));
+        System.out.println(dst2.get(2));
+    }
+
+    public static void main(String[] args) throws IOException {
+        demo();
+//        serializesPOJO();
     }
 }
