0b758b72c50d79001cdc7ead09b54bb3106e7515
guanjian23 <guanjian@jd.com>
2021-01-19 09:16:44 +0800

    feat(io): java nio

--- src/main/java/com/github/java/learning/nio/ByteBufferTest.java
+++ src/main/java/com/github/java/learning/nio/ByteBufferTest.java
@@ -6,32 +6,9 @@ import java.nio.ByteBuffer;
  * created by guanjian on 2021/1/11 9:53
  */
 public class ByteBufferTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ByteBuffer bb = ByteBuffer.allocateDirect(10);
 
-        ByteBuffer bb = ByteBuffer.allocate(10);
-        System.out.println(bb);
-        bb.put((byte)1);
-        System.out.println(bb);
-        bb.put((byte)2);
-        System.out.println(bb);
-        bb.put((byte)3);
-        System.out.println(bb);
-        bb.put((byte)4);
-        System.out.println(bb);
-        bb.put((byte)5);
-        System.out.println(bb);
-
-        bb.flip();
-        System.out.println(bb);
-        System.out.println(bb.get());
-        System.out.println(bb);
-        System.out.println(bb.get());
-        System.out.println(bb);
-        System.out.println(bb.get());
-        System.out.println(bb);
-        System.out.println(bb.get());
-        System.out.println(bb);
-        System.out.println(bb.get());
-        System.out.println(bb);
+        Thread.sleep(500000);
     }
 }
