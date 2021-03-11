30237d9cf55ef91b4a2afd261ae83b09edf546e2
guanjian <guanjian@jd.com>
2021-03-11 14:05:42 +0800

    feat(test) test

--- src/main/java/com/github/java/learning/ApiTest.java
+++ src/main/java/com/github/java/learning/ApiTest.java
@@ -11,14 +11,67 @@ public class ApiTest {
     public static void test01() {
         IntStream.rangeClosed(0, 30).forEach(x -> {
             int num = new Double(Math.pow(2, x)).intValue() - 1;
-            System.out.format("\t 2的%s次方 num:%s \t binary:%s \n",x, num, Long.toBinaryString(num));
+            System.out.format("\t 2的%s次方 num:%s \t binary:%s \n", x, num, Long.toBinaryString(num));
         });
     }
 
 
     public static void main(String[] args) {
-        String binary = "111111111111111111111111111111111000011100100010000110010001110";
+//        int[][] nums = new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+//        System.out.println(findNumberIn2DArray(nums, 5));
+//
+//        int[][] nums2 = new int[][]{{-5}};
+//        System.out.println(findNumberIn2DArray(nums2, -5));
 
-        System.out.println(Long.toBinaryString(Long.MAX_VALUE));
+        System.out.println(replaceSpace("We are happy."));
+    }
+
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        //{0,0},{0,1},{0,2}....
+        //{1,0},{1,1},{1,2}....
+        //{2,0},{2,1},{2,2}....
+
+        //{x,y},{x,y+1},{x,y+2}
+        //{x+1,y},{x+1,y+1},{x+1,y+2}
+        //{x+2,y},{x+2,y+1},{x+2,y+2}
+
+        int x = 0, y = 0;
+
+        while (x <= matrix.length -1 && y <= matrix[x].length - 1) {
+            System.out.format("x=%s y=%s matrix=%s target=%s \n", x, y, matrix[x][y],target);
+
+            if (target == matrix[x][y] || target == matrix[x+1][y] || target == matrix[x][y+1]) return true;
+
+            //右子树
+            else if (target < matrix[x+1][y]) {
+                x++;
+            }
+            //左子树
+            else if (target > matrix[x][y+1]) {
+                y++;
+            }
+        }
+
+        return false;
+    }
+
+    public static String replaceSpace(String s) {
+        //扩容替换文本长度的倍数，避免扩容或溢出
+        char[] ret = new char[3 * s.length()];
+
+        int index = 0;
+        //遍历字符串
+        for(int i = 0 ; i < s.length(); i ++){
+            char c = s.charAt(i);
+            index = i;
+            if( c == ' '){
+                ret[index++] = '%';
+                ret[index++] = '2';
+                ret[index++] = '0';
+            }else{
+                ret[index] = c;
+            }
+        }
+        return new String(ret);
     }
 }
--- src/test/java/com/github/test/TransactionTest.java
+++ src/test/java/com/github/test/TransactionTest.java
@@ -2,6 +2,7 @@ package com.github.test;
 
 import com.github.java.learning.spring.transaction.DiyTransactionSynchronization;
 import org.junit.Test;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -12,8 +13,39 @@ import javax.annotation.Resource;
  */
 public class TransactionTest extends BaseTest {
 
-    @Resource
+    @Resource(name = "transactionTemplate")
     private TransactionTemplate transactionTemplate;
+
+    @Test
+    public void test_tx_propagation_required() {
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        boolean result = transactionTemplate.execute(transactionStatus -> {
+            try {
+                 boolean nested = test_nested_tx();
+                System.out.println(" inner result = " + nested);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                transactionStatus.setRollbackOnly();
+                return false;
+            }
+        });
+
+        System.out.println("outter result = " + result);
+    }
+
+    private boolean test_nested_tx(){
+        return transactionTemplate.execute(transactionStatus -> {
+            try {
+                throw new RuntimeException();
+                //return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                transactionStatus.setRollbackOnly();
+                return false;
+            }
+        });
+    }
 
     @Test
     public void test_registerSynchronization_succ() {
