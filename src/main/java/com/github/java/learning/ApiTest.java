385ba91de50058f771744b0371567d8835ae0b93
guanjian23 <guanjian@jd.com>
2021-03-12 12:23:40 +0800

    feat(算法): 数组

--- src/main/java/com/github/java/learning/ApiTest.java
+++ src/main/java/com/github/java/learning/ApiTest.java
@@ -19,16 +19,20 @@ public class ApiTest {
 
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(()->{
-            try {
-                Thread.sleep(10000000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        //设置守护线程，则守护主线程，main方法结束则退出；
-        //如果不设置，则为用户线程，阻塞在sleep上
-        t1.setDaemon(true);
-        t1.start();
+//        Thread t1 = new Thread(()->{
+//            try {
+//                Thread.sleep(10000000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        //设置守护线程，则守护主线程，main方法结束则退出；
+//        //如果不设置，则为用户线程，阻塞在sleep上
+//        t1.setDaemon(true);
+//        t1.start();
+        int a = 10;
+        System.out.println(a^a);
+        System.out.println(a^0);
+        System.out.println(a^a^a);
     }
 }
---
+++ src/main/java/com/github/java/learning/dataStructure/array/FindDuplicateInArray.java
@@ -0,0 +1,58 @@
+package com.github.java.learning.dataStructure.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * created by guanjian on 2021/3/12 11:24
+ */
+public class FindDuplicateInArray {
+
+    /**
+     * 集合
+     * <p>
+     * 空间复杂度 O(N)
+     * 时间复杂度 O(N)
+     */
+    public static int findDuplicateBySet(int[] arr) {
+        if (null == arr || 0 == arr.length) return -1;
+
+        Set set = new HashSet();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(arr[i])) {
+                return arr[i];
+            } else {
+                set.add(arr[i]);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 异或
+     * <p>
+     *     a^0 =a
+     *     a^a =0
+     *
+     * 无需额外空间
+     * 时间复杂度 O(N)
+     */
+    public static int findDuplicateXOR(int[] arr) {
+        if (null == arr || 0 == arr.length) return -1;
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result ^= arr[i];
+        }
+
+        if (0 != result) return result;
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 2, 5, 6, 1, 2, 10, 7, 9};
+        System.out.println(findDuplicateXOR(arr));
+    }
+}
