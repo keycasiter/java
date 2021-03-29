46cf08bf386de145cd59cd0bb2c16983c33efcea
guanjian23 <guanjian@jd.com>
2021-03-29 09:33:43 +0800

    feat(limiter): 限流

--- src/main/java/com/github/java/learning/guava/RateLimiterDemo.java
+++ src/main/java/com/github/java/learning/guava/RateLimiterDemo.java
@@ -10,19 +10,13 @@ import java.util.stream.IntStream;
  */
 public class RateLimiterDemo {
 
-    private final static RateLimiter switchRateLimiter = RateLimiter.create(10, 2, TimeUnit.MILLISECONDS);
+    private final static RateLimiter switchRateLimiter = RateLimiter.create(10, 10, TimeUnit.MILLISECONDS);
 
     public static void main(String[] args) {
-        System.out.format("rate = %s", switchRateLimiter.getRate());
-        //尝试获取一个执行的机会，成功返回true，失败的话则立即false，不排队，想排队用acquire
-        IntStream.rangeClosed(0,2).forEach(x->{
-            switchRateLimiter.tryAcquire();
-        });
+        RateLimiter r = RateLimiter.create(5);
+        while (true) {
 
-        if (switchRateLimiter.tryAcquire()) {
-            System.out.println("执行");
-        } else {
-            System.out.println("拦截");
+            System.out.println("get 1 tokens: " + r.acquire() + "s");
         }
     }
 }
